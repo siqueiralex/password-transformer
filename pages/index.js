33ce09b34de6d1c9e5fe91f89f0cb3658e9e6abe
Alex Siqueira Lacerda <lequizi@gmail.com>
2020-12-14 21:15:06 -0300

    minor adjustments

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { sha256 } from 'js-sha256';
 import React, {useState, useEffect} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faCopy } from "@fortawesome/free-solid-svg-icons";
+
+const minus = "abcdefghijklmnopwrstuvwxyz"
+const maius = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const num = "0123456789"
+const espec = "!@#$%^&*()"
 
 export default function Home() {
 
@@ -12,8 +15,8 @@ export default function Home() {
 
   const [phrase, setPhrase] = useState('');
   const [password, setPassword] = useState('');
-  const [passwordShown, setPasswordShown] = useState(true);
-  const [phraseShown, setPhraseShown] = useState(true);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [phraseShown, setPhraseShown] = useState(false);
 
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
@@ -22,133 +25,9 @@ export default function Home() {
     setPhraseShown(phraseShown ? false : true);
   };
 
-  const minus = "abcdefghijklmnopwrstuvwxyz"
-  const maius = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  const num = "0123456789"
-  const espec = "!@#$%^&*()"
-  
-
-  function shuffle(ar,seed){
-    let numbers = [];
-    for( let a = 0, max = ar.length; a < max; a++){
-        numbers.push(a);
-    }
-    let shuffled = [];
-    for( let i = 0, len = ar.length; i < len; i++ ){
-        let r = (seed *1000)% (len - i) ;
-        shuffled.push(ar[numbers[r]]);
-        numbers.splice(r,1);
-    }
-    return shuffled.join("");
-  }
-  
-
   useEffect( () => {
-    let passletters = ""
-    let c = ""
-    let hash = sha256.create();
-    hash.update(phrase);
-    const hashed_word = hash.hex();
-    let i=0;
-    
-    let idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
-    if (idx<0){
-      c = minus.charAt(minus.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = minus.charAt(idx)
-      passletters = passletters.concat(minus.charAt(idx))
-    }
-    
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
-    if (idx<0){
-      c = minus.charAt(minus.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = minus.charAt(idx)
-      passletters = passletters.concat(minus.charAt(idx))
-    }
-  
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%maius.length - 1
-    if (idx<0){
-      c = maius.charAt(maius.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = maius.charAt(idx)
-      passletters = passletters.concat(maius.charAt(idx))
-    }
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%maius.length - 1
-    if (idx<0){
-      c = maius.charAt(maius.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = maius.charAt(idx)
-      passletters = passletters.concat(maius.charAt(idx))
-    }
-  
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%num.length - 1
-    if (idx<0){
-      c = num.charAt(num.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = num.charAt(idx)
-      passletters = passletters.concat(num.charAt(idx))
-    }
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%num.length - 1
-    if (idx<0){
-      c = num.charAt(num.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = num.charAt(idx)
-      passletters = passletters.concat(num.charAt(idx))
-    }
-  
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%espec.length - 1
-    if (idx<0){
-      c = espec.charAt(espec.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = espec.charAt(idx)
-      passletters = passletters.concat(espec.charAt(idx))
-    }
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%espec.length - 1
-    if (idx<0){
-      c = espec.charAt(espec.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = espec.charAt(idx)
-      passletters = passletters.concat(espec.charAt(idx))
-    }
-  
-  
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
-    if (idx<0){
-      c = minus.charAt(minus.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = minus.charAt(idx)
-      passletters = passletters.concat(minus.charAt(idx))
-    }
-    i+=2
-    idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
-    if (idx<0){
-      c = minus.charAt(minus.length + idx)
-      passletters = passletters.concat(c)      
-    }else{
-      c = minus.charAt(idx)
-      passletters = passletters.concat(minus.charAt(idx))
-    }
-    i+=2
-    let seed = parseInt("0x"+hashed_word.slice(i, i+2))
-    setPassword(shuffle(passletters,seed)) 
+
+    setPassword(transform(phrase)) 
 
   });  
 
@@ -157,22 +36,20 @@ export default function Home() {
     <div className="container">
 
         <div className="generator">
-          <p>
-            <form>
+            <div className="form">
 
-              <label htmlFor="phrase">Secret Phrase</label>
+              <label htmlFor="phrase">Weak Password</label>
               <div>
 
               <input 
               type = {phraseShown ? "text" : "password"}
               id="phrase" 
-              placeholder="Easy-to-remember secret phrase" 
+              placeholder="Your easy-to-remember password" 
               value = {phrase}
               onChange={ e => setPhrase(e.target.value) }/>
 
               <i onClick={togglePhraseVisiblity}>{eye}</i>
               </div>
-              <button className="btn" type="submit">Generate</button>
               
               
                 <label htmlFor="phrase">Strong Password</label>
@@ -186,12 +63,137 @@ export default function Home() {
                 />
                 <i onClick={togglePasswordVisiblity}>{eye}</i>
                 <i onClick={() => {navigator.clipboard.writeText(password)}} >{copyIcon}</i>
-              </div>             
-              
-            </form>
-          </p>
+              </div>
+            </div>             
         </div>
  
       </div>
   )
+}
+
+function shuffle(ar,seed){
+  let numbers = [];
+  for( let a = 0, max = ar.length; a < max; a++){
+      numbers.push(a);
+  }
+  let shuffled = [];
+  for( let i = 0, len = ar.length; i < len; i++ ){
+      let r = (seed *1000)% (len - i) ;
+      shuffled.push(ar[numbers[r]]);
+      numbers.splice(r,1);
+  }
+  return shuffled.join("");
+}
+
+function transform(phrase){
+  if(phrase){
+
+      let passletters = ""
+      let c = ""
+      let hash = sha256.create();
+      hash.update(phrase);
+      const hashed_word = hash.hex();
+      let i=0;
+      
+      let idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
+      if (idx<0){
+        c = minus.charAt(minus.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = minus.charAt(idx)
+        passletters = passletters.concat(minus.charAt(idx))
+      }
+      
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
+      if (idx<0){
+        c = minus.charAt(minus.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = minus.charAt(idx)
+        passletters = passletters.concat(minus.charAt(idx))
+      }
+    
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%maius.length - 1
+      if (idx<0){
+        c = maius.charAt(maius.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = maius.charAt(idx)
+        passletters = passletters.concat(maius.charAt(idx))
+      }
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%maius.length - 1
+      if (idx<0){
+        c = maius.charAt(maius.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = maius.charAt(idx)
+        passletters = passletters.concat(maius.charAt(idx))
+      }
+    
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%num.length - 1
+      if (idx<0){
+        c = num.charAt(num.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = num.charAt(idx)
+        passletters = passletters.concat(num.charAt(idx))
+      }
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%num.length - 1
+      if (idx<0){
+        c = num.charAt(num.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = num.charAt(idx)
+        passletters = passletters.concat(num.charAt(idx))
+      }
+    
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%espec.length - 1
+      if (idx<0){
+        c = espec.charAt(espec.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = espec.charAt(idx)
+        passletters = passletters.concat(espec.charAt(idx))
+      }
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%espec.length - 1
+      if (idx<0){
+        c = espec.charAt(espec.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = espec.charAt(idx)
+        passletters = passletters.concat(espec.charAt(idx))
+      }
+    
+    
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
+      if (idx<0){
+        c = minus.charAt(minus.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = minus.charAt(idx)
+        passletters = passletters.concat(minus.charAt(idx))
+      }
+      i+=2
+      idx = parseInt("0x"+hashed_word.slice(i, i+2))%minus.length - 1
+      if (idx<0){
+        c = minus.charAt(minus.length + idx)
+        passletters = passletters.concat(c)      
+      }else{
+        c = minus.charAt(idx)
+        passletters = passletters.concat(minus.charAt(idx))
+      }
+      i+=2
+      let seed = parseInt("0x"+hashed_word.slice(i, i+2))
+      
+      return shuffle(passletters,seed)
+  }
+  return ""
 }
