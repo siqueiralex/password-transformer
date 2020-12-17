4a5e771f95e09e00405fef109109e567a1099847
Alex Siqueira Lacerda <lequizi@gmail.com>
2020-12-17 11:57:32 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -37,14 +37,16 @@ export default function Home() {
 
 
   return (
-    <div className="container">
-        <Head>
+    <>
+    <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>  
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <title>Password Transformer</title>
           
         </Head>
+      <div className="container">
         <div className="generator">
             <div className="form">
 
@@ -79,6 +81,7 @@ export default function Home() {
         </div>
  
       </div>
+      </>
   )
 }
 
