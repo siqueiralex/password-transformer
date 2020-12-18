7ca0888b3112bc97328c6b45899cbbc9a444276a
Alex Siqueira Lacerda <lequizi@gmail.com>
2020-12-18 11:41:02 -0300

    site title

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <title>Password Transformer</title>
+          <title>PassTry</title>
           
         </Head>
       <div className="container">
